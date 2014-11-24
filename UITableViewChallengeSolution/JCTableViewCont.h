58bc4ebd06245cd9f0d9c7f3d24a47e8306d89b6
Jagmeet <thalia@Thalias-iMac.local>
2014-11-23 22:05:22 -0800

    solve the challenge

---
+++ UITableViewChallengeSolution/JCTableViewCont.h
@@ -0,0 +1,13 @@
+//
+//  JCTableViewCont.h
+//  UITableViewChallengeSolution
+//
+//  Created by Jagmeet on 11/23/14.
+//  Copyright (c) 2014 Think Ninja Cut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCTableViewCont : UITableViewController
+
+@end
